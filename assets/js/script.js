0490e646e0d945d3a4d5be01d41911ccffb297cd
DanDeLuna <danieldeluna4@gmail.com>
2023-03-19 12:09:47 -0600

    Went through the whole code to make sure ID matched in Java and HTML sheets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
 
+const questions = [
+    {
+        question: "Who is the main character in Cowboy Bebop ?",
+        choices: ["a. Goku", "b. Monkey D. Luffy ", "c. Levi Ackerman", "d. Spike Spiegel"],
+        answer: "d. Spike Spiegel"
+    },
+    { 
+        question: "Who is the main character in Naruto ?",
+        choices: ["a. Tanjiro", "b. Gon Freecss", "c. Naruto", "d. Edward Elric"],
+        answer: "c. Naruto"
+    },
+    {
+        question: "Who is the main character in Dragon Ball Z ?",
+        choices: ["a. Levi Ackerman", "b. Goku", "c. Astro Boy", "d. Tanjiro"],
+        answer: "b. Goku"
+    },
+    {
+        question: "Who is the main character in Hunter x Hunter ?",
+        choices: ["a. Astro Boy ", "b. Levi Ackerman", "c. Gon Freecss", "d. Jotaro Kujo"],
+        answer: "c. Gon Freecss"
+    },
+    {
+        question: "Who is the main character in Berserk ?",
+        choices: ["a. Levi Ackerman", "b. Guts", "c. Jotaro Kujo", "d. Roronoa Zoro"],
+        answer: "b. Guts"
+    },
+    {
+        question: "Who is the main character in Demon Slayer?",
+        choices: ["a. Edward Elric", "b. Levi Ackerman", "c. Tanjiro", "d. Astro Boy"],
+        answer: "c. Tanjiro"
+    },
+    {
+        question: "Who is the main character in Death Note ?",
+        choices: ["a. Gon Freecss", "b. L", "c. Edward Elric", "d. Astro Boy"],
+        answer: "b. L"
+    },
+    {
+        question: "Who is the main character in Fullmetal Alchemist ?",
+        choices: ["a. Edward Elric", "b. Tanjiro", "c. Astro Boy", "d. Jotaro Kujo"],
+        answer: "a. Edward Elric"
+    }
+];
 // grab references to elements
 var timer = document.getElementById("timer");
 var timeLeft = document.getElementById("timeLeft");
